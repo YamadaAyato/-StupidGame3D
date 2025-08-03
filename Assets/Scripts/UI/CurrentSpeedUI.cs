73e3af98ec1a63bbac5f21e8980cd31580b74bc7
ayato0913a <ayato0913a@gmail.com>
2025-08-03 23:44:18 +0900

    [update]スピードに応じて大きさが変わる機能を追加

--- Assets/Scripts/UI/CurrentSpeedUI.cs
+++ Assets/Scripts/UI/CurrentSpeedUI.cs
@@ -1,15 +1,26 @@
 using TMPro;
 using UnityEngine;
+using DG.Tweening;
 using UnityEngine.UI;
+
+
 
 public class CurrentSpeedUI : MonoBehaviour
 {
     [SerializeField] private PlayerController playerController;
     [SerializeField] private TMP_Text _speedText;
+
+    private Tween _scaleText;
     
     void Update()
     {
         float zSpeed = playerController.CurrentZSpeed;
         _speedText.text = zSpeed.ToString("F2");
+        
+        float targetScale = Mathf.Clamp(1f +  Mathf.Abs(zSpeed) / 20f, 1f, 1.5f);
+
+        _scaleText?.Kill();
+        _scaleText = _speedText.rectTransform
+            .DOScale(targetScale, 0.2f);
     }
 }
